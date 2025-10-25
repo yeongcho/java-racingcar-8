847e87f02ce8a438a53ea49f7b6d3b1460c750f3
yeongcho <yeoncho17@naver.com>
2025-10-25 21:01:44 +0900

    refactor: Application.java의 입력 기능 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,8 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //1. 사용자 입력 기능
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String carNames = Console.readLine();
 
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String tryCount = Console.readLine();
     }
 }
